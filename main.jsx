4e2645ce85a052af1d879cc44ca5169d1ee1150c
ValiedeKhan <154665693+ValiedeKhan@users.noreply.github.com>
2025-04-11 11:33:04 +0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Web3CV from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Web3CV />
+  </React.StrictMode>
+);
